e45bd0aedcc3099812cde2bdd5bea613bb2cac0b
Shawn Stolsig <shawnstolsig@gmail.com>
2020-04-27 16:09:18 -0700

    starting

--- react-hooks/hackernoon-clone/app/index.js
+++ react-hooks/hackernoon-clone/app/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { ThemeProvider } from './contexts/theme'
+import ThemeContext, { ThemeProvider } from './contexts/theme'
 import Loading from './components/Loading'
 import Nav from './components/Nav'
 
@@ -10,45 +10,42 @@ const Posts = React.lazy(() => import('./components/Posts'))
 const Post = React.lazy(() => import('./components/Post'))
 const User = React.lazy(() => import('./components/User'))
 
-class App extends React.Component {
-  state = {
-    theme: 'light',
-    toggleTheme: () => {
-      this.setState(({ theme }) => ({
-        theme: theme === 'light' ? 'dark' : 'light'
-      }))
-    }
-  }
-  render() {
-    return (
-      <Router>
-        <ThemeProvider value={this.state}>
-          <div className={this.state.theme}>
-            <div className='container'>
-              <Nav />
+function App(){
+  const [theme, setTheme] = React.useState('light')
 
-              <React.Suspense fallback={<Loading />}>
-                <Switch>
-                  <Route
-                    exact
-                    path='/'
-                    render={() => <Posts type='top' />}
-                  />
-                  <Route
-                    path='/new'
-                    render={() => <Posts type='new' />}
-                  />
-                  <Route path='/post' component={Post} />
-                  <Route path='/user' component={User} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
-          </div>
-        </ThemeProvider>
-      </Router>
-    )
+  const toggleTheme = () => {
+    setTheme((theme) => theme === 'light' ? 'dark' : 'light')
   }
+
+  return (
+    <Router>
+      <ThemeProvider value={theme}>
+        <div className={theme}>
+          <div className='container'>
+            <Nav toggleTheme={toggleTheme}/>
+
+            <React.Suspense fallback={<Loading />}>
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={() => <Posts type='top' />}
+                />
+                <Route
+                  path='/new'
+                  render={() => <Posts type='new' />}
+                />
+                <Route path='/post' component={Post} />
+                <Route path='/user' component={User} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
+            </React.Suspense>
+          </div>
+        </div>
+      </ThemeProvider>
+    </Router>
+  )
+
 }
 
 ReactDOM.render(
