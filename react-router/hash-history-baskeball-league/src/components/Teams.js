c9b22b0626232f9243e3b2af599b43ec0b3e8d00
Shawn Stolsig <shawnstolsig@gmail.com>
2020-04-30 16:26:23 -0700

    basic Team page implemented

--- react-router/hash-history-baskeball-league/src/components/Teams.js
+++ react-router/hash-history-baskeball-league/src/components/Teams.js
@@ -1,9 +1,95 @@
 import React from 'react'
+import { Route, Link } from 'react-router-dom'
+import Sidebar from './Sidebar'
+import { getTeamNames, getTeam } from '../api'
+import { parse } from 'query-string'
+import slug from 'slug'
 
-export default function Teams(){
+function teamReducer(state, action){
+    if(action.type==='loading'){
+        return {
+            teamNames: [],
+            loading: true
+        }
+    } else if (action.type==="success"){
+        return {
+            loading: false,
+            teamNames: action.teams
+        }
+    } else {
+        throw new Error("action not supported")
+    }
+}
+
+export default function Teams(props){
+    const {location, match} = props
+
+    const [{teamNames, loading}, dispatch] = React.useReducer(teamReducer, {
+        teamNames: [],
+        loading: true,
+    })
+
+    const fetchTeamNames = () => {
+        dispatch({type: 'loading'})
+        getTeamNames()
+            .then((teams) => dispatch({type: 'success', teams}))
+    }
+
+    React.useEffect(()=> {
+        location.search 
+            ? fetchTeamNames(parse(location.search).teamId)
+            : fetchTeamNames()
+    },[location])
+
     return (
         <div className="container two-column">
-            <h1>Teams Page</h1>
+            <Sidebar
+                loading={loading}
+                title="Teams"
+                list={teamNames.map((team) => team)}
+                {...props}
+            />
+
+            {!loading && location.pathname === '/teams'
+                ? <div className='sidebar-instruction'>Select a Team</div>
+                : null }
+
+            {/* <Route 
+                path={`${match.url}/:playerId`}
+                render={({match})=>{
+                    if(loading) return null
+
+                    const { 
+                        name, position, teamId, number, avatar, apg, ppg, rpg, spg
+                    } = players.find((player) => slug(player.name) === match.params.playerId)
+                    
+                    return (
+                        <div className="panel">
+                            <img className='avatar' src={`${avatar}`} alt={`Avatar for ${name}`} />
+                            <h1 className='medium-header'>{name}</h1>
+                            <h3 className='header'>#{number}</h3>
+                            <div className="row">
+                                <ul className='info-list' style={{marginRight: 80}}>
+                                    <li>
+                                        Team 
+                                        <div>
+                                            <Link style={{color: '#68809a'}} to={`/${teamId}`}>{teamId[0].toUpperCase() + teamId.slice(1)}</Link>
+                                        </div>
+                                    </li>
+                                    <li>Position<div>{position}</div></li>
+                                    <li>PPG<div>{ppg}</div></li>
+                                </ul>
+                                <ul className="info-list">
+                                    <li>APG<div>{apg}</div></li>
+                                    <li>SPG<div>{spg}</div></li>
+                                    <li>RPG<div>{rpg}</div></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    )
+                }}
+            /> */}
         </div>
     )
 }
